2cafa79041d7a3416c697da7adf15c2a9a8ae829
Lyndon Darwin-Shelton <45465765+Lyndondshelton@users.noreply.github.com>
2019-10-01 13:59:51 -0400

    Update FizzBuzz.java
    Add comments

--- FizzBuzz/src/com/lds/FizzBuzz/FizzBuzz.java
+++ FizzBuzz/src/com/lds/FizzBuzz/FizzBuzz.java
@@ -3,14 +3,22 @@ package com.lds.FizzBuzz;
 public class FizzBuzz {
 	
 	public static void main(String[] args) {
+		//loop through numbers 1-100
 		for(int i=1; i<=100; i++) {
+			//First check to see if the number is divisible by 3 or 5, if it is divisible by both 3 and 5 then print "FizzBuzz".
 			if((i%3) == 0 && (i%5) == 0) {
 				System.out.printf("FizzBuzz\n");
-			}else if((i%5) == 0) {
+			}
+			//check to see if the number is divisible by 5, if it is then print "Buzz".
+			else if((i%5) == 0) {
 				System.out.printf("Buzz\n");
-			}else if((i%3) == 0) {
+			}
+			//check to see if the number is divisible by 3, if it is then print "Fizz".
+			else if((i%3) == 0) {
 				System.out.printf("Fizz\n");
-			}else {
+			}
+			//If the number is not divisible by 3 or 5 then print the number.
+			else {
 				System.out.printf("%d\n",i);
 			}
 		}
